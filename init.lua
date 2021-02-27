262594eaefbacabde7f71426b71cb9aa67ea3cf6
Jan Andrle <andrle.jan@centrum.cz>
2021-02-27 18:06:43 +0100

    :tada:

---
+++ init.lua
@@ -0,0 +1,3 @@
+-- Bacuse of `gates_long` mod (also see corresponding textures)
+minetest.register_alias("default:pine_wood", "lottplants:pinewood")
+minetest.register_alias("default:acacia_wood", "lottplants:alderwood")
